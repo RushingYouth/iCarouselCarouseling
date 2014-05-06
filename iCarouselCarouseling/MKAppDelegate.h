3322a8668731ce1567a2227d8ab0a3daf9411a03
RushingYouth <rushingyouth@Gmail.com>
2014-05-06 17:46:40 +0800

    Initial Commit

---
+++ iCarouselCarouseling/MKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MKAppDelegate.h
+//  iCarouselCarouseling
+//
+//  Created by lanou3g on 14-5-6.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iCarouselCarouseling/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iCarouselCarouseling
+//
+//  Created by lanou3g on 14-5-6.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MKAppDelegate class]));
+    }
+}
